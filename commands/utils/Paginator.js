9422ebe6e9a443db2ab023bef5137c1a2486e4dd
Raphael <duqueraphaa@gmail.com>
2026-05-02 19:27:18 -0300

    02/05/2026 19:27:17

--- commands/utils/Paginator.js
+++ commands/utils/Paginator.js
@@ -15,34 +15,34 @@ class Paginator {
     this.index = 0
     this.time = time
     this.ownerId = null
-    this.ended = false
   }
 
-  buildRow() {
+  buildRow(disabled = false) {
     return new ActionRowBuilder().addComponents(
       new ButtonBuilder()
         .setCustomId(`prev:${this.ownerId}`)
         .setEmoji(icon.leftarrow)
-        .setStyle(ButtonStyle.Primary),
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(disabled),
 
       new ButtonBuilder()
         .setCustomId(`next:${this.ownerId}`)
         .setEmoji(icon.rightarrow)
         .setStyle(ButtonStyle.Primary)
+        .setDisabled(disabled)
     )
   }
 
   render() {
-  const page = this.pages[this.index]
-  
-    // se for função → executa passando contexto
+    const page = this.pages[this.index]
+
     if (typeof page === 'function') {
       return page({
         actualPage: this.index + 1,
         totalPages: this.pages.length
       })
     }
-  
+
     return page
   }
 
@@ -54,6 +54,7 @@ class Paginator {
       components: [this.buildRow()],
       withResponse: true
     })
+
     const msg = response.resource.message
 
     const collector = msg.createMessageComponentCollector({
@@ -61,40 +62,42 @@ class Paginator {
     })
 
     collector.on('collect', async i => {
-      const [action, ownerId] = i.customId.split(':')
+      try {
+        const [action, ownerId] = i.customId.split(':')
 
-      if (i.user.id !== ownerId) {
-        return i.reply({
-          content: `${icon.error} **|** Os botões não são seus!`,
-          flags: MessageFlags.Ephemeral
-        })
-      }
-      
-      if (this.ended) {
-        return i.reply({
-          content: `${icon.error} **|** Os dados da interação sumiram, use o comando novamente!`,
-          flags: MessageFlags.Ephemeral
-        })
-      }
+        if (i.user.id !== ownerId) {
+          return i.reply({
+            content: `${icon.error} **|** Os botões não são seus!`,
+            flags: MessageFlags.Ephemeral
+          }).catch(() => {})
+        }
 
-      if (action === 'prev') {
-        this.index--
-        if (this.index < 0) this.index = this.pages.length - 1
-      }
+        if (action === 'prev') {
+          this.index--
+          if (this.index < 0) this.index = this.pages.length - 1
+        }
 
-      if (action === 'next') {
-        this.index++
-        if (this.index >= this.pages.length) this.index = 0
-      }
+        if (action === 'next') {
+          this.index++
+          if (this.index >= this.pages.length) this.index = 0
+        }
 
-      await i.update({
-        embeds: [this.render()],
-        components: [this.buildRow()]
-      })
+        await i.update({
+          embeds: [this.render()],
+          components: [this.buildRow()]
+        }).catch(() => {})
+
+      } catch (err) {
+        console.log('Paginator error:', err)
+      }
     })
-    
-    collector.on('end', () => {
-      
+
+    collector.on('end', async () => {
+      try {
+        await msg.edit({
+          components: [this.buildRow(true)]
+        })
+      } catch {}
     })
   }
 }

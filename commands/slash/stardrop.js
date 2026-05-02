60ad85eb3aaa3bd165b047c34723549bf63407b6
Raphael <duqueraphaa@gmail.com>
2026-05-02 10:55:51 -0300

    02/05/2026 10:55:51 - Tirei o teste

--- commands/slash/stardrop.js
+++ commands/slash/stardrop.js
@@ -17,8 +17,6 @@ module.exports = {
         .setDescription('「 Diversão 」Ver os Brawlers dos Star Drops Mafios')
     ),
   
-  test: true,
-  
   async execute(interaction) {
     const sub = interaction.options.getSubcommand()
     const subcommand = interaction.client.subcommands.get(`star_drop.${sub}`)

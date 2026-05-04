605c2682f60f428a781517d6fe706af75ccc950a
Raphael <duqueraphaa@gmail.com>
2026-05-04 16:08:26 -0300

    04/05/2026 16:08:26 - isManager() agora é inteiramente utilizado com client.managers

--- commands/utils/isManager.js
+++ commands/utils/isManager.js
@@ -4,14 +4,7 @@ const filePath = path.join(__dirname, '../../json/botManagers.json')
 
 
 function isManager(client, id) {
-  let botManagers = []
-  
-  try {
-    botManagers = JSON.parse(fs.readFileSync(filePath, 'utf8'))
-  } catch {
-    botManagers = []
-  }
-  return botManagers.some(user => user?.id === id)
+  return client.managers.get(id) ? true : false
 }
 
 module.exports = { isManager }

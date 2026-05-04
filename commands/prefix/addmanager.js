44a2af5921c4bcba8cd05edad4872b699b913bee
Raphael <duqueraphaa@gmail.com>
2026-05-04 16:37:40 -0300

    04/05/2026 16:37:39 - Agora &addmamnager funciona com client.managers

--- commands/prefix/addmanager.js
+++ commands/prefix/addmanager.js
@@ -1,14 +1,16 @@
 const path = require('path')
 const fs = require('fs')
-const fileJSONPath = path.join(__dirname, '../../json/botManagers.json')
+const managersJsonPath = path.join(__dirname, '../../json/botManagers.json')
 const { EmbedBuilder } = require('discord.js')
 const { isKapu } = require('../utils/isKapu.js')
 const { correctUseEmbed } = require('../utils/correctUseEmbed.js')
+const { saveManagers } = require('../utils/saveManagers.js')
 const { sendManagerEdit } = require('../utils/sendManagerEdit.js')
 const { getEmojis } = require('../utils/getEmojis.js')
 
 module.exports = {
-  name: "addmanager",
+  name: "add.manager",
+  aliases: ['addmanager'],
   
   async execute(msg, args) {
     if (!msg.guild) return
@@ -32,41 +34,28 @@ module.exports = {
       msg.mentions.users.first() ||
       await msg.client.users.fetch(user).catch(() => null)
   
-    if (!userObj) {
+    if (!user) {
       return msg.reply({ embeds: [embedCorrectUse] }).catch(console.error)
     }
   
-    if (userObj.id === '1173408263920951356') {
+    if (user.id === '1173408263920951356') {
       return msg.reply('🤔 **|** Ei! Esse é você, e você é o meu dono!')
     }
   
-    let botManagers = []
-  
-    try {
-      botManagers = JSON.parse(fs.readFileSync(fileJSONPath, "utf8"))
-    } catch {
-      botManagers = []
-    }
-  
-    if (!Array.isArray(botManagers)) botManagers = []
-  
-    if (botManagers.some(manager => manager.id === userObj.id)) {
-      return msg.reply(`${icon.error || ':x:'} **|** O usuário de ID \`${userObj.id}\` já é \`MANAGER\`!`)
+    if (client.managers.get(user.id)) {
+      return msg.reply(`${icon.error || ':x:'} **|** O usuário de ID \`${user.id}\` já é \`MANAGER\`!`)
     }
   
     if (userName.length > 30) {
-      return msg.reply(`${icon.error || ':x:'} **|** Nome muito grande.`)
+      return msg.reply(`${icon.error || ':x:'} **|** Não são permitidos nomes com mais de 30 caracteres.`)
     }
   
-    botManagers.push({
-      name: userName,
-      id: userObj.id
-    })
+    client.managers.set(user.id, userName)
   
-    fs.writeFileSync(fileJSONPath, JSON.stringify(botManagers, null, 2))
+    await saveManagers(client, managersJsonPath)
   
-    sendManagerEdit(msg, userObj, true)
+    // await sendManagerEdit(msg, user, true)
   
-    msg.reply(`${icon.success || ':white_check_mark:'} **|** O(a) usuário(a) de ID \`${userObj.id}\` foi adicionado(a) como \`MANAGER\`!`)
+    msg.reply(`${icon.success || ':white_check_mark:'} **|** O(a) usuário(a) de ID \`${user.id}\` foi adicionado(a) como \`MANAGER\`!`)
   }
 }
---
+++ commands/utils/saveManagers.js
@@ -0,0 +1,40 @@
+const fs = require('fs/promises')
+
+let writing = false
+let pending = false
+
+async function saveManagers(client, managersPath) {
+  if (writing) {
+    pending = true
+    return
+  }
+
+  writing = true
+
+  try {
+    do {
+      pending = false
+      
+      let managers = []
+      for (const [id, name] of client.managers) {
+        managers.push({
+          name,
+          id
+        })
+      }
+
+      await fs.writeFile(
+        rngBrawlersPath,
+        JSON.stringify(managers, null, 2)
+      )
+
+    } while (pending)
+
+  } catch (err) {
+    console.error('Erro ao salvar managers:', err)
+  } finally {
+    writing = false
+  }
+}
+
+module.exports = { saveManagers }

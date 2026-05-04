732e648afb77fc6f4d194655b94a552a1412c0d0
Raphael <duqueraphaa@gmail.com>
2026-05-04 17:17:08 -0300

    04/05/2026 17:17:08 - Corrigi erro de syntax

--- commands/prefix/addserver.js
+++ commands/prefix/addserver.js
@@ -41,7 +41,7 @@ module.exports = {
       ).catch(() => null)
     }
   
-    client.allowGuilds.set(serverId)
+    client.allowGuilds.add(serverId)
   
     await saveAllowGuilds(client, allowGuildsJsonPath)
   

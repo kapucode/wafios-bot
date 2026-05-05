dbd9b69695f30224f119931385f89678da0551b1
Raphael <duqueraphaa@gmail.com>
2026-05-05 15:14:01 -0300

    05/05/2026 15:14:00

--- commands/slash/rng/ranking.js
+++ commands/slash/rng/ranking.js
@@ -2,12 +2,15 @@ module.exports = {
   name: 'rng.ranking',
   
   async execute(interaction, client) {
-    const sortedRng = client.rngBrawlers.sort((a, b) => b[1].rebirths - a[1].rebirths)
-    const top10 = array.slice(0, 10)
+    const sortedRng = Object.entries(client.rngBrawlers)
+     .sort((a, b) => b[1].rebirths - a[1].rebirths)
+    const top10 = sortedRng.slice(0, 10)
     const ranking = top10.map(([id, data], i) => {
       return `${i + 1}. <@${id}> - ${data.xp} XP`
     }).join('\n')
     
     console.log(ranking)
+    
+    interaction.reply(`foi`)
   }
 }

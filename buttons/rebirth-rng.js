1df4cb13a2752cb2a73e4bbb4f670e9dd4b3c255
Raphael <duqueraphaa@gmail.com>
2026-05-05 13:34:43 -0300

    05/05/2026 13:34:42

--- buttons/rebirth-rng.js
+++ buttons/rebirth-rng.js
@@ -52,7 +52,7 @@ module.exports = {
 - 🎯 Quantidade de rebirths atualmente: **${userRng.rebirths}**
 
 > 💫 Bônus de rebirth:
-- ${getLuckRng(userRng).multiplier + 1} sorte
+- ${getLuckRng(...userRng.rebirths++).multiplier} sorte
 - Cargos exclusivos
 - Chance de entrar no ranking (\`/rng rebirth ranking\`)`)
     

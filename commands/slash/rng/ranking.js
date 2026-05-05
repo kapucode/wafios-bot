65783dde04601306a47d069ada77451031d641fb
Raphael <duqueraphaa@gmail.com>
2026-05-05 15:15:43 -0300

    05/05/2026 15:15:43

--- commands/slash/rng/ranking.js
+++ commands/slash/rng/ranking.js
@@ -6,11 +6,9 @@ module.exports = {
      .sort((a, b) => b[1].rebirths - a[1].rebirths)
     const top10 = sortedRng.slice(0, 10)
     const ranking = top10.map(([id, data], i) => {
-      return `${i + 1}. <@${id}> - ${data.xp} XP`
+      return `${i + 1}. <@${id}> - ${data.rebirths} Rebirths`
     }).join('\n')
     
-    console.log(ranking)
-    
-    interaction.reply(`foi`)
+    interaction.reply(ranking)
   }
 }

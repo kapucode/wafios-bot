816a6931ae1c1741186280c8147dd5a4d7025c55
Raphael <duqueraphaa@gmail.com>
2026-05-05 15:27:45 -0300

    05/05/2026 15:27:45 - /rng ranking style createx

--- commands/slash/rng/ranking.js
+++ commands/slash/rng/ranking.js
@@ -1,14 +1,45 @@
+const Paginator = require('../../utils/Paginator.js')
+const {
+  EmbedBuilder
+} = require('discord.js')
+
 module.exports = {
   name: 'rng.ranking',
-  
+
   async execute(interaction, client) {
-    const sortedRng = Object.entries(client.rngBrawlers)
-     .sort((a, b) => b[1].rebirths - a[1].rebirths)
-    const top10 = sortedRng.slice(0, 10)
-    const ranking = top10.map(([id, data], i) => {
-      return `${i + 1}. <@${id}> - ${data.rebirths} Rebirths`
-    }).join('\n')
+
+    // 1. pegar e ordenar
+    const sorted = Object.entries(client.rngBrawlers || {})
+      .sort((a, b) => b[1].rebirths - a[1].rebirths)
+      .slice(0, 100)
+
+    // 2. montar páginas (strings)
+    const pagesRanking = []
+
+    for (let i = 0; i < sorted.length; i += 10) {
+      const chunk = sorted.slice(i, i + 10)
+
+      const pageText = chunk.map(([id, data], index) => {
+        const position = i + index + 1
+        return `${position}. <@${id}> - ${data.rebirths} rebirths`
+      }).join('\n')
+
+      pagesRanking.push(pageText)
+    }
     
-    interaction.reply(ranking)
+    const pages = pagesRanking.map((str) =>
+      ({ actualPage, totalPages }) =>
+        new EmbedBuilder()
+          .setTitle(`🎯 | Rebirth Ranking (${actualPage}/${totalPages})`)
+          .setDescription(str)
+          .setColor(Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0'))
+    )
+    const disabledBtn = pagesRanking.length <= 1
+    
+    const paginator = new Paginator({
+      pages,
+      disabledBtn
+    })
+    await paginator.start(interaction)
   }
 }

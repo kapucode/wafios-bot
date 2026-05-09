0193f9aa540985c5d86df4eb4c6f79f3313bf7b7
Raphael <duqueraphaa@gmail.com>
2026-05-08 22:59:28 -0300

    08/05/2026 22:59:28

--- commands/prefix/rng-inventario.js
+++ commands/prefix/rng-inventario.js
@@ -17,7 +17,7 @@ const rngBrawlersPath = path.join(__dirname, '../json/rngBrawlers.json')
 module.exports = {
   name: 'rng.inventario',
   aliases: ['rng.inv', 'rng.backpack', 'rng.bp'],
-  prefixes: ['+', '&', '.', ',']
+  prefixes: ['+', '&', '.', ','],
 
   async execute(msg, args) {
     const client = msg.client
